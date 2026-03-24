939648110022f65974965d8c6e96537e93cbaea9
latina001 <nannaphat20160@gmail.com>
2026-03-24 10:42:11 +0700

    edited code magnus

--- Assets/Sc/MagnusEffect.cs
+++ Assets/Sc/MagnusEffect.cs
@@ -3,35 +3,34 @@ using UnityEngine.InputSystem;
 
 public class MagnusEffect : MonoBehaviour
 {
-    public float kickForce = 0f;
-   public float spinAmount = 0f;
-   public float magnusStrenght = 0f;
+    public float kickforce = 0f;
+    public float spinAmount = 0f;
+    public float magnusStrength = 0f;
     private Rigidbody _rb;
-    private bool _isShoot = false;
+    private bool _isShoot;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();    
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame && !_isShoot) 
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !_isShoot)
         {
-            _rb.AddForce(-Vector3.forward * kickForce, ForceMode.Impulse);
-            _rb.AddForce(Vector3.up * spinAmount);
-            _isShoot =true;
-                
+            _rb.AddForce(new Vector3(0,0.5f,0) * kickforce, ForceMode.Impulse);
+            _rb.AddTorque(Vector3.up * spinAmount);
+            _isShoot = true;
         }
-
     }
     private void FixedUpdate()
     {
-        if (_isShoot) return;
+        if (!_isShoot) return;
         Vector3 velocity = _rb.linearVelocity;
-        Vector3 spin = _rb.linearVelocity;
+        Vector3 spin = _rb.angularVelocity;
 
-        Vector3 magnusForce = magnusStrenght * Vector3.Cross(spin , velocity);
+        Vector3 magnusForce = magnusStrength * Vector3.Cross(spin, velocity);
         _rb.AddForce(magnusForce);
     }
 }
